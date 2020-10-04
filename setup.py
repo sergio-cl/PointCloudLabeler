6e61b7a8e98486e547b4cdd903a45aaac901a1fd
sergio-cl <72324846+sergio-cl@users.noreply.github.com>
2020-10-04 18:52:55 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+form setuptools import setup
+
+setup(
+  name='PointCloudLabeler',
+  version='0.0.1',
+  description='PointCloudLabeler',
+  py_modules=["Annotator3D"],
+  package_dir={'': 'src'},
+)
